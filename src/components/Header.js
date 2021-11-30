674fcea3005a060a8231a0bf6e30a73f45fec048
prime-infinity <soe.a.prime@gmail.com>
2021-11-30 22:51:04 +0100

    using props types for more robust code

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,5 @@
+import PropTypes from 'prop-types'
+
 const Header = ({title}) =>{
 
     return (
@@ -7,5 +9,9 @@ const Header = ({title}) =>{
     );
   }
   
+  Header.propTypes = {
+      title:PropTypes.string.isRequired
+  }
+
   export default Header;
   
