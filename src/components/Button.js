7caa4037656fdfa506b9ca004637f056a55fa665
prime-infinity <soe.a.prime@gmail.com>
2021-12-01 19:59:36 +0100

    components

---
+++ src/components/Button.js
@@ -0,0 +1,5 @@
+const Button = ({color, text}) => {
+    return <button style={{backgroundColor:color}} className="btn">{text}</button>
+}
+
+export default Button
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,28 @@
 import PropTypes from 'prop-types'
+import Button from './Button'
 
 const Header = ({title}) =>{
 
     return (
       <header>
-          <h1 style={{color:'red',backgroundColor:'blue'}}>{title}</h1>
-      </header>
+          <h1>{title}</h1>
+          <Button color='green' text='hello' /> 
+          <Button color='blue' text='hello' /> 
+          <Button color='aqua' text='hello' /> 
+          <Button color='black' text='hello' /> 
+      </header>  
     );
   }
   
   Header.propTypes = {
       title:PropTypes.string.isRequired   
   }
+
+  //css in js
+  /*const headingStyle = {
+      color:'red',
+      backgroundColor:'blue',
+  }*/
 
   export default Header;
   
