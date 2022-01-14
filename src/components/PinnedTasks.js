bea430a0e87c74052e7093b16f454a5ab3936c23
prime-infinity <soe.a.prime@gmail.com>
2022-01-14 02:07:48 +0100

    done multiple cards

--- src/components/PinnedTasks.js
+++ src/components/PinnedTasks.js
@@ -1,3 +1,12 @@
+import { Swiper } from 'swiper/react/swiper';
+import { SwiperSlide } from 'swiper/react/swiper-slide';
+import { Pagination} from 'swiper';
+import { EffectCards } from 'swiper';
+
+import '../views/swiper.min.css';
+import 'swiper/modules/pagination/pagination.min.css'
+import 'swiper/modules/effect-cards/effect-cards.min.css';
+
 const PinnedTasks = () => {
     return (
 
@@ -11,56 +20,39 @@ const PinnedTasks = () => {
                 
             </div> 
             
-
-            <div className="col-12">
-
-                <div className="row flex-row flex-nowrap pb-4 pb-md-5 no-scroll-bar overflow-x-auto">
-
-                    <div className="col-12 col-md-8 col-lg-5 text-break">
-                        <div className="card bg-card-theme py-2">
-                            <div className="card-body pt-5 pb-3">
-                                <div className="row text-center">
-                                    <div className="col mr-2">
-                                        <div className=" font-weight-bold  mb-1 h3 text-muted-native"><span>Do Something else</span></div>
-                                        <div className="text-theme"><span>11-21-20</span></div>
-                                        <p className="text-muted-native">this is a small note attaced</p>
+            <Swiper
+                modules={[Pagination,EffectCards]}
+                spaceBetween={1}
+                slidesPerView={1}
+                pagination={{ clickable: true }}
+                effect="cards"
+                grabCursor={true}
+            >
+                {[1, 2, 3, 4].map((i, el) => {
+                    return (
+                        <SwiperSlide key={el}>
+                            
+                            <div className="col-12 text-break">
+                                <div className="card bg-card-theme py-2">
+                                    <div className="card-body pt-5 pb-3">
+                                        <div className="row text-center">
+                                            <div className="col mr-2">
+                                                <div className=" font-weight-bold  mb-1 h3 text-muted-native"><span>Do Something else</span></div>
+                                                <div className="text-theme"><span>11-21-20 {el}</span></div>
+                                                <p className="text-muted-native">this is a small note attaced</p>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                        </div>
-                    </div>
+                            </div> 
 
-                    <div className="col-12 col-md-8 col-lg-5 text-break">
-                        <div className="card bg-card-theme py-2">
-                            <div className="card-body pt-5 pb-3">
-                                <div className="row text-center">
-                                    <div className="col mr-2">
-                                        <div className=" font-weight-bold  mb-1 h3 text-muted-native"><span>Do Something else</span></div>
-                                        <div className="text-theme"><span>11-21-20</span></div>
-                                        <p className="text-muted-native">this is a small note attaced</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                        </SwiperSlide>
+                    )
+                    
+                })}
+                
+            </Swiper>
 
-                    <div className="col-12 col-md-8 col-lg-5 text-break">
-                        <div className="card bg-card-theme py-2">
-                            <div className="card-body pt-5 pb-3">
-                                <div className="row text-center">
-                                    <div className="col mr-2">
-                                        <div className=" font-weight-bold  mb-1 h3 text-muted-native"><span>Do Something else</span></div>
-                                        <div className="text-theme"><span>11-21-20</span></div>
-                                        <p className="text-muted-native">this is a small note attaced</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                </div>
-
-            </div>
         </div>
     )
 }
