d63c10102e3cc4e718309626d16148d42c5f855e
prime-infinity <soe.a.prime@gmail.com>
2022-01-20 22:51:49 +0100

    changed home icons color

--- src/components/AllTasks.js
+++ src/components/AllTasks.js
@@ -5,20 +5,39 @@ const AllTasks = () => {
         
         <div className="row">
 
-            <div className="col-12 mb-4 mt-4 mb-4 text-theme">
+            <div className="col-12 mb-4 mt-4 mb-4 text-theme p-0">
                 <span className="h3 font-weight-bold">All Tasks</span>
             </div>
 
             {Array.from(Array(10), (e, i) => { return(
-            <div className="col-12 col-md-6 mb-5" key={i}>
+            <div className="col-12 col-md-6 mb-5 p-0 pl-md-1 pr-md-1" key={i}>
 
                 <div className="card bg-card-theme py-2 text-muted">
 
-                    <div className="card-header">
+                    
+
+                    <div className="card-body">
 
                         <div className="row">
 
-                            <div className="col-8 text-theme">
+                            <div className="col-12 text-break text-center">
+                                <span className="h3 font-weight-bold">Do someThing Like That</span>
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                    <div className="card-footer pb-0 pt-0 text-muted">
+                        <div className="row">
+                            {/*<div className="col-8">
+                                
+                                <svg xmlns="http://www.w3.org/2000/svg"  width="1em" height="1em" viewBox="0 0 20 20" fill="currentColor" style={{fontSize: '25px'}}>
+                                    <path fillRule="evenodd" d="M18 13V5a2 2 0 00-2-2H4a2 2 0 00-2 2v8a2 2 0 002 2h3l3 3 3-3h3a2 2 0 002-2zM5 7a1 1 0 011-1h8a1 1 0 110 2H6a1 1 0 01-1-1zm1 3a1 1 0 100 2h3a1 1 0 100-2H6z" clipRule="evenodd" />
+                                </svg>
+                               
+                            </div>*/}
+                            <div className="col-2 text-theme">
                                 
                                 {
                                     i <= 2 ? <svg xmlns="http://www.w3.org/2000/svg"  width="1em" height="1em" fill="none" viewBox="0 0 24 24" stroke="currentColor" style={{fontSize: '25px'}}>
@@ -38,43 +57,17 @@ const AllTasks = () => {
                                 }
                                 
                             </div>
-
-                            <div className="col-4 text-right">
-
-                            <svg xmlns="http://www.w3.org/2000/svg" cwidth="1em" height="1em" viewBox="0 0 20 20" fill="currentColor" style={{fontSize: '25px'}}>
-                                <path fillRule="evenodd" d="M3 17a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm3.293-7.707a1 1 0 011.414 0L9 10.586V3a1 1 0 112 0v7.586l1.293-1.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z" clipRule="evenodd" />
-                            </svg>
-
-                            </div>
-
-                        </div>
-
-                    </div>
-
-                    <div className="card-body">
-
-                        <div className="row">
-
-                            <div className="col-12 text-break text-center">
-                                <span className="h3 font-weight-bold">Do someThing Like That</span>
-                            </div>
-
-                        </div>
-
-                    </div>
-
-                    <div className="card-footer text-muted">
-                        <div className="row">
                             <div className="col-8">
-                                
-                                <svg xmlns="http://www.w3.org/2000/svg"  width="1em" height="1em" viewBox="0 0 20 20" fill="currentColor" style={{fontSize: '25px'}}>
-                                    <path fillRule="evenodd" d="M18 13V5a2 2 0 00-2-2H4a2 2 0 00-2 2v8a2 2 0 002 2h3l3 3 3-3h3a2 2 0 002-2zM5 7a1 1 0 011-1h8a1 1 0 110 2H6a1 1 0 01-1-1zm1 3a1 1 0 100 2h3a1 1 0 100-2H6z" clipRule="evenodd" />
-                                </svg>
-                               
+
                             </div>
-                            <div className="col-4 text-right">
+                            {/*<div className="col-2 text-right">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 20 20" fill="currentColor" style={{fontSize: '25px'}}>
                                     <path fillRule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clipRule="evenodd" />
+                                </svg>
+                            </div>*/}
+                            <div className="col-2 text-right">
+                                <svg xmlns="http://www.w3.org/2000/svg" className="" width="1em" height="1em" viewBox="0 0 20 20" fill="currentColor" style={{fontSize: '25px'}}>
+                                    <path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z" />
                                 </svg>
                             </div>
                         </div>
