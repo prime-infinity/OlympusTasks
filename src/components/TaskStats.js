2acf050f8069fbc91e4cb3850a0688180ebaf3a0
prime-infinity <soe.a.prime@gmail.com>
2022-01-20 22:23:53 +0100

    changed home icons color

--- src/components/TaskStats.js
+++ src/components/TaskStats.js
@@ -13,7 +13,7 @@ const TaskStast = () => {
                         <div className="row justify-content-center text-center pt-md-4 pb-md-4">
                             <div className="col-6">
 
-                                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 footer-icon-selected" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 13l4 4L19 7" />
                                 </svg>
 
@@ -22,10 +22,10 @@ const TaskStast = () => {
                     </div>
                     <div className="card-footer text-muted">
                         <div className="row">
-                            <div className="col-9">
+                            <div className="col-12">
                                 Done
                             </div>
-                            <div className="col-4 text-right text-theme abs-right">
+                            <div className="col-6 text-right text-theme abs-right">
                                 22
                             </div>
                         </div>
@@ -39,7 +39,7 @@ const TaskStast = () => {
                         <div className="row justify-content-center text-center pt-md-4 pb-md-4">
                             <div className="col-6">
                                 
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="footer-icon-selected" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1} d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
                             </svg>
 
@@ -48,10 +48,10 @@ const TaskStast = () => {
                     </div>
                     <div className="card-footer text-muted">
                         <div className="row">
-                            <div className="col-9">
+                            <div className="col-12">
                                 Pending
                             </div>
-                            <div className="col-4 text-right text-theme abs-right">
+                            <div className="col-6 text-right text-theme abs-right">
                                 7
                             </div>
                         </div>
@@ -65,7 +65,7 @@ const TaskStast = () => {
                         <div className="row justify-content-center text-center pt-md-4 pb-md-4">
                             <div className="col-6 ">
 
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 footer-icon-selected" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1} d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15" />
                             </svg>
 
@@ -74,10 +74,10 @@ const TaskStast = () => {
                     </div>
                     <div className="card-footer text-muted">
                         <div className="row">
-                            <div className="col-9">
+                            <div className="col-12">
                                 Ongoing
                             </div>
-                            <div className="col-4 text-right text-theme abs-right">
+                            <div className="col-6 text-right text-theme abs-right">
                                 10
                             </div>
                         </div>
@@ -91,7 +91,7 @@ const TaskStast = () => {
                         <div className="row justify-content-center text-center pt-md-4 pb-md-4">
                             <div className="col-6 ">
 
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 footer-icon-selected" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1} d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
                             </svg>
 
@@ -100,10 +100,10 @@ const TaskStast = () => {
                     </div>
                     <div className="card-footer ">
                         <div className="row">
-                            <div className="col-9">
+                            <div className="col-12">
                                 In Review
                             </div>
-                            <div className="col-4 text-right text-theme abs-right">
+                            <div className="col-6 text-right text-theme abs-right">
                                 12
                             </div>
                         </div>
