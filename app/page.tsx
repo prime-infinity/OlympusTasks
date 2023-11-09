0adaafce60abce64094359d902cc10eb02984e0c
prime-infinity <soe.a.prime@gmail.com>
2023-11-09 02:46:10 -0800

    test deployment from preview branch

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import SideBar from "./components/navigation/SideBar";
 export default function Home() {
   return (
     <>
-      <div>now, go to /home</div>
+      <div>go to /home</div>
     </>
   );
 }
