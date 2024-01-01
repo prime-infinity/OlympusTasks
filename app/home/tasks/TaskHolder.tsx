6041412e77dbacddade52849c0485e879acd1fcb
prime-infinity <soe.a.prime@gmail.com>
2024-01-01 01:45:37 -0800

    done getting tasks from indexeddb into all tasks page

--- app/home/tasks/TaskHolder.tsx
+++ app/home/tasks/TaskHolder.tsx
@@ -1,14 +1,20 @@
-import { tasks } from "@/app/data";
+"use client";
 import Tasks from "./Tasks";
+import { taskDB } from "@/app/helpers/indexedDB";
+import { useLiveQuery } from "dexie-react-hooks";
 
 export default function TaskHolder() {
+  const taskList = useLiveQuery(() => taskDB.tasks.toArray(), []);
+
   return (
     <>
       <div className="col-span-1 md:col-span-2 border">TaskHolder</div>
-      {tasks.map((task) => (
+      {taskList?.map((task) => (
         <Tasks key={task.id} data={task} />
       ))}
-      {tasks.length === 0 && <p className="text-center">There are no tasks</p>}
+      {(!taskList || taskList.length === 0) && (
+        <p className="text-center">There are no tasks</p>
+      )}
     </>
   );
 }
